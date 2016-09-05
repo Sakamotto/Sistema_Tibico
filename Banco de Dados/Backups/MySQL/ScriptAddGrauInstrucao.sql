943dd59301c49997c75e4305340b0b1f0a7d6af4
Danilo de Oliveira <danilo.psn94@gmail.com>
2016-09-05 19:17:06 -0300

    Modelo do banco e backups atualizados + transformações

---
+++ Banco de Dados/Backups/MySQL/ScriptAddGrauInstrucao.sql
@@ -0,0 +1,7 @@
+INSERT INTO grauinstrucao (descricao) VALUES ('Técnico');
+INSERT INTO grauinstrucao (descricao) VALUES ('Superior');
+INSERT INTO grauinstrucao (descricao) VALUES ('Pós-Graduação');
+INSERT INTO grauinstrucao (descricao) VALUES ('Mestrado');
+INSERT INTO grauinstrucao (descricao) VALUES ('Doutorado');
+
+SELECT * FROM grauinstrucao;
